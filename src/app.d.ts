1f3e04eaa7dbffdb69b4d0c250b8fd2c5b002efd
Kabir Goel <kabirgoel.kg@gmail.com>
2023-03-17 04:49:22 -0700

    chore: Upgrade to latest SvelteKit

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,12 +1,14 @@
-/// <reference types="@sveltejs/kit" />
-
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
-declare namespace App {
-	// interface Locals {}
-	// interface Platform {}
-	// interface Session {}
-	// interface Stuff {}
+declare global {
+	namespace App {
+		// interface Error {}
+		// interface Locals {}
+		// interface PageData {}
+		// interface Platform {}
+	}
 }
 
 declare module 'prism-svelte';
+
+export {};
