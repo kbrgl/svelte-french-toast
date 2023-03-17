c59dedd97ad6e007bdffbe42b9dbca753fb62c14
Kabir Goel <kabirgoel.kg@gmail.com>
2023-03-17 04:22:40 -0700

    chore: Use Playwright in latest SvelteKit skeleton

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,8 +3,9 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 const config: PlaywrightTestConfig = {
 	webServer: {
 		command: 'npm run build && npm run preview',
-		port: 3000
-	}
+		port: 4173
+	},
+	testDir: 'tests'
 };
 
 export default config;
