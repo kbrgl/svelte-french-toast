87a0334ee49244be04b20e5169ab6e6ec48989eb
nbe <aleisa.naser@gmail.com>
2023-06-29 09:46:59 +0300

    remove unused type

--- tests/components/boilerplate/types.ts
+++ tests/components/boilerplate/types.ts
@@ -1,5 +1,3 @@
 import type toast from '$lib';
-import type { test } from '@playwright/experimental-ct-svelte';
 
-export type Mount = Parameters<Parameters<typeof test>[1]>['0']['mount'];
 export type ToastProps = Parameters<typeof toast>;
