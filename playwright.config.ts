98c5a29a86699edb1ad33830591aac07caf7079f
nbe <aleisa.naser@gmail.com>
2023-06-29 03:50:53 +0300

    ci: create test workflow

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,6 +18,14 @@ export default defineConfig({
 		{
 			name: 'chromium',
 			use: { ...devices['Desktop Chrome'] }
+		},
+		{
+			name: 'firefox',
+			use: { ...devices['Desktop Firefox'] }
+		},
+		{
+			name: 'webkit',
+			use: { ...devices['Desktop Safari'] }
 		}
 	]
 });
