cea80de0c0fbe913f922e52e91986f77bf39392f
Kabir Goel <kabirgoel.kg@gmail.com>
2025-02-22 00:08:28 -0800

    use eslint flat config

---
+++ eslint.config.js
@@ -0,0 +1,32 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintPluginSvelte from 'eslint-plugin-svelte';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default tseslint.config(
+	eslint.configs.recommended,
+	tseslint.configs.recommended,
+	eslintPluginSvelte.configs.recommended,
+	eslintConfigPrettier.recommended,
+	{
+		ignorePatterns: ['*.cjs'],
+		overrides: [
+			{
+				files: ['*.svelte'],
+				parser: 'svelte-eslint-parser',
+				parserOptions: {
+					parser: '@typescript-eslint/parser'
+				}
+			}
+		],
+		parserOptions: {
+			sourceType: 'module',
+			ecmaVersion: 2020
+		},
+		env: {
+			browser: true,
+			es2017: true,
+			node: true
+		}
+	},
+);
