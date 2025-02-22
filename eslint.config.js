efe46c0aa871545aa9299962979ed8ec3389308b
Kabir Goel <kabirgoel.kg@gmail.com>
2025-02-22 00:51:28 -0800

    get eslint and prettier working

--- eslint.config.js
+++ eslint.config.js
@@ -1,32 +1,52 @@
 import eslint from '@eslint/js';
+import globals from 'globals';
 import tseslint from 'typescript-eslint';
+import tsParser from '@typescript-eslint/parser';
 import eslintPluginSvelte from 'eslint-plugin-svelte';
+import svelteParser from 'svelte-eslint-parser';
 import eslintConfigPrettier from 'eslint-config-prettier';
+import svelteConfig from './svelte.config.js';
 
 export default tseslint.config(
 	eslint.configs.recommended,
 	tseslint.configs.recommended,
-	eslintPluginSvelte.configs.recommended,
-	eslintConfigPrettier.recommended,
+	eslintPluginSvelte.configs['flat/recommended'],
+	eslintConfigPrettier,
 	{
-		ignorePatterns: ['*.cjs'],
-		overrides: [
-			{
-				files: ['*.svelte'],
-				parser: 'svelte-eslint-parser',
-				parserOptions: {
-					parser: '@typescript-eslint/parser'
-				}
-			}
-		],
-		parserOptions: {
+		ignores: [
+			'*.cjs',
+			'.DS_Store',
+			'node_modules',
+			'/build',
+			'/.svelte-kit',
+			'/package',
+			'.env',
+			'.env.*',
+			'!.env.example',
+			'pnpm-lock.yaml',
+			'package-lock.json',
+			'yarn.lock'
+		]
+	},
+	{
+		languageOptions: {
 			sourceType: 'module',
-			ecmaVersion: 2020
-		},
-		env: {
-			browser: true,
-			es2017: true,
-			node: true
+			ecmaVersion: 2020,
+			globals: {
+				...globals.browser,
+				...globals.es2017,
+				...globals.node
+			}
+		}
+	},
+	{
+		files: ['*.svelte', '**/*.svelte'],
+		languageOptions: {
+			parser: svelteParser,
+			parserOptions: {
+				parser: tsParser,
+				svelteConfig
+			}
 		}
 	}
 );
